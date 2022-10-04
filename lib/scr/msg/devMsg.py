f6d0acae4fcc2e424610b167cbddc1f6f2028151
Toadman <111460078+The-ToadCoder@users.noreply.github.com>
2022-10-04 14:33:18 -0600

    Create devMsg.py

---
+++ lib/scr/msg/devMsg.py
@@ -0,0 +1,10 @@
+#Congrats you found the hidden message
+MSG = """
+Here is a quick message from the creator
+
+      Enjoy the fun and addictive game 
+    of eating toads, with a nice crunch
+    sound!
+      
+      -Toadman
+"""
