0ba0cd0679e6bff7582f090e4353f19bb63e2624
Toadman <111460078+The-ToadCoder@users.noreply.github.com>
2022-10-04 14:32:15 -0600

    Create webLinks.py
    Update/ finish later

---
+++ lib/scr/webLinks.py
@@ -0,0 +1,18 @@
+import webbrowser
+
+
+def main():
+    contacts()
+
+
+def contacts():
+    replitURL = ''
+    githubURL = ''
+    scratchURL = ''
+    twitchURL = ''
+    youtubeURL = ''
+    discordURL = ''
+
+
+if __name__ == '__main__':
+    main()
