0fc61e48c3ffd7723c10f6a73f7bb478ffe7326d
Toadman <111460078+The-ToadCoder@users.noreply.github.com>
2022-10-04 14:29:45 -0600

    Create config.py

---
+++ lib/config/config.py
@@ -0,0 +1,35 @@
+#    Pygame Window Settings
+#Game window width
+WINWIDTH = 680
+#Game window height
+WINHEIGHT = 420
+
+#    Player Settings
+#Player's Starting size
+STARTSIZE = 30
+#Player's health/lives (default value is 3 lives)
+MAXHEALTH = 3
+#Player's Speed
+MOVERATE = 9
+#Player's Bounce height
+BOUNCEHEIGHT = 30
+#Player's Bounce rate
+BOUNCERATE = 6
+
+#    Global Settings
+#Frames per second
+FPS = 30
+#Number of Grass objects in game area (not the camera/feild of view area)
+NUMGRASS = 80
+#The size goal (the size the player needs to win the game)
+WINSIZE = 300
+
+#    Toad Settings
+#Number of Toads spawned in the game area
+NUMTOADS = 30
+#Toads min speed
+TOADMINSPEED = 3
+#Toads max speed
+TOADMAXSPEED = 7
+#Frequency of the toads changing direction
+DIRCHANGEFREQ = 2
