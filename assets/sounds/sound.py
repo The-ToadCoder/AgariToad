bd403cb71ea0375756ba816694142ed3594a765f
Toadman <111460078+The-ToadCoder@users.noreply.github.com>
2022-10-04 14:26:26 -0600

    Create sound.py

---
+++ assets/sounds/sound.py
@@ -0,0 +1,6 @@
+soundPath = 'assets/sounds/'
+backgroundSoundFile = 'backgroundMusic.wav'
+loseSoundFile = 'Lose.wav'
+hurtSoundFile = 'hurt.wav'
+winSoundFile = 'Win.wav'
+crunchSoundFile = 'crunch.wav'
